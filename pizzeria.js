e023296c3d9e10046a6efbb5949809b2fa9ea39f
Francisca Estefanía Martínez Lozano <fany.marloz@gmail.com>
2022-03-07 00:02:01 -0600

    Proyecto Pizzeria

---
+++ pizzeria.js
@@ -0,0 +1,13 @@
+window.onload=function(){
+    const selectElement=
+    document.forms[0].categoria;
+
+    const container =
+    document.getElementById("container");
+
+    const container2 =
+    document.getElementById("Sabor");
+
+       
+    
+}
